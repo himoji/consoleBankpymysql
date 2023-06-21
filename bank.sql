bdcf70a5e9f7dd7069c0e4d8ed73d14c38e69f4a
himoji <61588258+himoji@users.noreply.github.com>
2023-06-21 20:16:06 +0600

    Create bank.sql

---
+++ bank.sql
@@ -0,0 +1,14 @@
+set autocommit = 0;
+create database customers_db;
+use customers_db;
+create table customers_table(
+	customer_id int unique,
+    customer_name varchar(16) not null,
+    customer_cash int default 0
+);
+insert into customers_table value
+	(1, "Mark Robert", 1000000),
+    (2, "Zack Chi", 205500),
+    (3, "admin", 999999);
+select * from customers_table;
+commit;
