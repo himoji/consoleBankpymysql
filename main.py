e439bc4478176fda87b53011f9dc65d9692c436c
himoji <61588258+himoji@users.noreply.github.com>
2023-06-21 21:25:30 +0600

    Update main.py 1.1

--- main.py
+++ main.py
@@ -34,10 +34,13 @@ class atm():
         '''
 
             customer_name = input('Who are you (your name)?: ')
-            cash = int(input('How much do you want to deposit (input integer)?: '))
+            try:
+                cash = int(input('How much do you want to deposit (input integer)?: '))
+            except:
+                print("Deposit only integers, ATM can not hold cents")
 
             cursor.execute(f'update customers_table set customer_cash = customer_cash + {cash} where customer_name = "{customer_name}";')
-            print(f'update customers_table set customer_cash = customer_cash + {cash} where customer_name = "{customer_name}";')
+            printCashAmount(customer_name)
 
 
         def withdraw() -> None:
@@ -48,10 +51,13 @@ class atm():
         '''
 
             customer_name = input('Who are you (your name)?: ')
-            cash = int(input('How much do you want to withdraw (input integer)?: '))
+            try:
+                cash = int(input('How much do you want to withdraw (input integer)?: '))
+            except:
+                print("Withdraw only integers, ATM can not hold cents")
 
             cursor.execute(f'update customers_table set customer_cash = customer_cash - {cash} where customer_name = "{customer_name}";')
-            #print(cursor.execute(f'select customer_cash from customers_table where customer_name = "{customer_name}";'))
+            printCashAmount(customer_name)
 
 
         def send() -> None:
@@ -63,24 +69,33 @@ class atm():
 
             sender = input('Who are you (your name)?: ')
             taker = input('Send money to who? (taker name): ')
-            cash = int(input('How much do you want to withdraw (input integer)?: '))
 
-            cursor.execute(f'update customers_table set customer_cash = customer_cash - {cash} where customer_name = "{sender}";')
-            cursor.execute(f'update customers_table set customer_cash = customer_cash + {cash} where customer_name = "1{taker}";')
-            #print(cursor.execute(f'select customer_cash from customers_table where customer_name = "{sender}";'), cursor.execute(f'select customer_cash from customers where customer_name = "{taker}";'))
+            try:
+                cash = int(input('How much do you want to withdraw (input integer)?: '))
+            except:
+                print("Only integers, ATM can not hold cents")
+
+            a = f'update customers_table set customer_cash = customer_cash - {cash} where customer_name = "{sender}";'
+            b = f'update customers_table set customer_cash = customer_cash + {cash} where customer_name = "{taker}";'
+
+            cursor.execute(a)
+            cursor.execute(b)
+
+            printCashAmount(sender)
+            printCashAmount(taker)
 
 
-        """def selectXfromY(x, y):
+        def printCashAmount(customer_name):
             '''
         Select command to print in terminal
-        Takes 2 positional args: x - what, y - out where
+        Print in terminal amount of money of CUSTOMER_NAME 
+        Takes 1 positional arg: customer_name e.g. Mark Robert
         '''
+            cursor.execute(f'select customer_cash from customers_table where customer_name = "{customer_name}";')
 
-            cursor.execute(f'select {x} from {y}')
-
-            result = cursor.fetchall()
+            result = cursor.fetchone()
             for i in result:
-                print(i)"""
+                print(f"{customer_name} cash is now {i}")
 
 
         def main() -> None:
@@ -91,19 +106,16 @@ class atm():
                 while True:
                     try:
                         match int(input('\n\n\n\n\n===ATM===\n1) Deposit\n2) Withdraw\n3) Send cash\n4) Exit\n')):
-                            case 1: deposit()
-                            case 2: withdraw()
-                            case 3: send()
+                            case 1: deposit(); input("Press ENTER")
+                            case 2: withdraw(); input("Press ENTER")
+                            case 3: send(); input("Press ENTER")
                             case 4: break # causes bug so user need to type 4 two times
-                            case _: print('Please enter only numbers from 1 to 4: ')
-                        cursor.execute("commit;")
-                    except: 
-                        print('Please enter only numbers (1-4): ')
+                    except Exception as err: 
+                        print(f'[BUG] {err}') # causes bug
+                    cursor.execute("commit;")
         main()
          
 
 if __name__ == "__main__":
     atmRunner = atm()
     atmRunner.__init__()
-
-
