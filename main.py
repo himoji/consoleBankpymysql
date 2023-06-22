1db3f6f96292d3715618420ef74aadaa75874819
himoji <61588258+himoji@users.noreply.github.com>
2023-06-22 21:47:28 +0600

    Update main.py 3.0
    proper oop

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ except Exception as err:
     print("Connection refused\n", err)
 
 
-def securityCheck(*args) -> bool:
+def securityCheck(*args) -> None:
     '''
     Checks user input for: blacklisted words, going out-of-bounds
 \n
@@ -63,228 +63,239 @@ def securityCheck(*args) -> bool:
 
 
 class accountManagment:
-    def __init__(self) -> int:
-        def login(cursor) -> str:
-            '''
-        Login command to take user input and check is it password.\n
-        takes 0 args, only asks inside of function.\n
-        returns customer_name and True if user input is correct
-            '''
-            customer_name = input("What is your name?: ").lower()
-            password = getpass.getpass("What is your password?: ")
+    def __init__(self) -> None:
+        self.main()
+    def login(self) -> None:
+        '''
+    Login command to take user input and check is it password.\n
+    takes 0 args, only asks inside of function.\n
+    returns customer_name and True if user input is correct
+        '''
+        customer_name = input("What is your name?: ").lower()
+        password = getpass.getpass("What is your password?: ")
 
-            cursor.execute(f'select customer_id from customers_table where customer_name = "{customer_name}" and customer_password = "{password}";')
-            result = cursor.fetchone()
+        self.cursor.execute(f'select customer_id from customers_table where customer_name = "{customer_name}" and customer_password = "{password}";')
+        result = self.cursor.fetchone()
 
-            try:  # causes bug, when everything is ok
-                print(result[0])
-                atma = atm(result[0]) # result[0] - customer_id
-                atma.__init__()
-                print("Logged in successfully")
-            except Exception as err:
-                print(f"Name or password is incorrect {err}")
+        try:  # causes bug, when everything is ok
+            print(result[0]) #type: ignore
+            print("Logged in successfully")
+            atma = atm(result[0]) # type: ignore result[0] - customer_id
+            atma.__init__(result[0]) # type: ignore
 
-            
-        def changePassword(cursor) -> None:
-            '''
-        ChngPass command to take user input and set it as password.\n
-        takes 0 args, only asks inside of function.
-            '''
-            customer_name = input("What is your name?: ").lower()
+        except Exception as err:
+            print(f"Name or password is incorrect {err}")
 
-            old_password = getpass.getpass("What is your password?: ")
-            new_password = getpass.getpass("What password do you want? (max: 20 chars): ")
-
-            cursor.execute(f'select customer_id from customers_table where customer_name = "{customer_name}" and customer_password = "{old_password}";')
-            result = cursor.fetchone()
-
-            try: 
-                result[0] #result[0] - customer_id: login check, if no user found - raises error, if found one - going futher
-                cursor.execute(f'update customers_table set customer_password = "{new_password}" where customer_name = "{customer_name}";')
-                print("Successfully changed password!")
-            except Exception as err:
-                print(f"Name or password is incorrect {err}")
-
-
-        def register(cursor) -> None:
-            '''
-        Register command to take user input and load it into customers_table.\n
-        takes 0 args, only asks inside of function.
-            '''
-
-            customer_name = input("What is your name? (max: 16 chars): ")
-            password = getpass.getpass("What password do you want? (max: 20 chars): ")
-
-            securityCheck(customer_name, password)
-
-            try:
-                cursor.execute(f'insert into customers_table(customer_name, customer_password) values ("{customer_name}", "{password}");')
-
-            except Exception as err:
-                print(err)
-
-            
-        def deleteAcc(cursor) -> None:
-            '''
-        Delete command to delete a row with specified id \n
-        takes 0 args, only asks inside of function.
-            '''
-            try:
-                customer_id = int(input("What is your id?: "))
-                sure = bool(int(input("Are you sure? (1 or 0)?: ")) == 1)
-
-            except Exception as err:
-                print(err)
-
-            if sure:
-                cursor.execute(f'delete from customers_table where customer_id = {customer_id};')
-
-
-        def main():
-                '''
-            Gui command to print auth menu in terminal.\n
-            Takes int 1-4 to reach other commands, if input is incorrect trying again.
-            '''
-                while True:
-                    try:
-                        cursor = db.cursor()
-
-                        match int(input('\n\n\n\n\n===ATM===\n1) Login\n2) Register\n3) Change password\n4) Delete account\n5) Exit\n')):
-                            case 1: login(cursor); input("Press ENTER")
-                            case 2: register(cursor); input("Press ENTER")
-                            case 3: changePassword(cursor); input("Press ENTER")
-                            case 4: deleteAcc(cursor); input("Press ENTER")
-                            case 5: exit()
-                        cursor.execute("commit;")
-                        cursor.close() # to avoid out-of-sync error
-                    except Exception as err: 
-                        print(f'[BUG] {err}')
-                    
         
+    def changePassword(self) -> None:
+        '''
+    ChngPass command to take user input and set it as password.\n
+    takes 0 args, only asks inside of function.
+        '''
+        customer_name = input("What is your name?: ").lower()
+
+        old_password = getpass.getpass("What is your password?: ")
+        new_password = getpass.getpass("What password do you want? (max: 20 chars): ")
+
+        self.cursor.execute(f'select customer_id from customers_table where customer_name = "{customer_name}" and customer_password = "{old_password}";')
+        result = self.cursor.fetchone()
+
+        try: 
+            result[0] #result[0] - customer_id: login check, if no user found - raises error, if found one - going futher
+            self.cursor.execute(f'update customers_table set customer_password = "{new_password}" where customer_name = "{customer_name}";')
+            print("Successfully changed password!")
+        except Exception as err:
+            print(f"Name or password is incorrect {err}")
+
+
+    def register(self) -> None:
+        '''
+    Register command to take user input and load it into customers_table.\n
+    takes 0 args, only asks inside of function.
+        '''
+
+        customer_name = input("What is your name? (max: 16 chars): ")
+        password = getpass.getpass("What password do you want? (max: 20 chars): ")
+
+        securityCheck(customer_name, password)
+
+        try:
+            self.cursor.execute(f'insert into customers_table(customer_name, customer_password) values ("{customer_name}", "{password}");')
+
+        except Exception as err:
+            print(err)
+
         
-        main()
+    def deleteAcc(self) -> None:
+        '''
+    Delete command to delete a row with specified id \n
+    takes 0 args, only asks inside of function.
+        '''
+        try:
+            customer_id = int(input("What is your id?: "))
+            sure = bool(int(input("Are you sure? (1 or 0)?: ")) == 1)
+
+        except Exception as err:
+            print(err)
+
+        if sure: #type: ignore
+            self.cursor.execute(f'delete from customers_table where customer_id = {customer_id};') #type: ignore
+
+
+    def main(self) -> None:
+            '''
+        Gui command to print auth menu in terminal.\n
+        Takes int 1-4 to reach other commands, if input is incorrect trying again.
+        '''
+            while True:
+                try:
+                    self.cursor = db.cursor()
+
+                    match int(input('\n\n\n\n\n===ATM===\n1) Login\n2) Register\n3) Change password\n4) Delete account\n5) Exit\n')):
+                        case 1: self.login(); input("Press ENTER")
+                        case 2: self.register(); input("Press ENTER")
+                        case 3: self.changePassword(); input("Press ENTER")
+                        case 4: self.deleteAcc(); input("Press ENTER")
+                        case 5: exit()
+                    self.cursor.execute("commit;")
+                    self.cursor.close() # to avoid out-of-sync error
+                except Exception as err: 
+                    print(f'[BUG] {err}')
 
 
 class atm():
     def __init__(self, current_customer_id) -> None:
-        def getCustomerNameById(cursor, current_customer_id):
-            cursor.execute(f'select customer_name from customers_table where customer_id = {current_customer_id};')
-            return cursor.fetchone()[0]
-        def deposit(cursor) -> None:
-            '''
-        'Deposit command to update CASH at customer_id.\n
-        Cash += cash_input for user.\n
-        takes 0 args, only asks inside of function.
+        self.current_customer_id = current_customer_id
+        self.main()
+
+
+    def getCustomerNameById(self):
+        self.cursor.execute(f'select customer_name from customers_table where customer_id = {self.current_customer_id};')
+        return self.cursor.fetchone()[0]
+    
+
+    def deposit(self) -> None:
         '''
-            try:
-                cash = int(input('How much do you want to deposit (input integer)?: '))
-                if cash < 0:
-                    print("You cant take debt here!")
-                    cash = str(cash)
-                    cash = "nope"
-            except:
-                print("Deposit Only integers and positive numbers, ATM can not hold cents")
+    'Deposit command to update CASH at customer_id.\n
+    Cash += cash_input for user.\n
+    takes 0 args, only asks inside of function.
+    '''
+        try:
+            cash = int(input('How much do you want to deposit (input integer)?: '))
+            if cash < 0:
+                print("You cant take debt here!")
+                cash = str(cash)
+                cash = "nope"
+        except:
+            print("Deposit Only integers and positive numbers, ATM can not hold cents")
 
-            cursor.execute(f'update customers_table set customer_cash = customer_cash + {cash} where customer_id = "{current_customer_id}";')
-            printCashAmountById(current_customer_id, cursor)
+        self.cursor.execute(f'update customers_table set customer_cash = customer_cash + {cash} where customer_id = "{self.current_customer_id}";') # type: ignore
+        self.printCashAmount()
 
 
-        def withdraw(cursor) -> None:
-            '''
-        Withdraw command to update CASH at customer_id.\n
-        Cash -= cash_input for user.\n
-        takes 0 args, only asks inside of function.
+    def withdraw(self) -> None:
         '''
-            try: #check how much money customer have
-                cursor.execute(f'select customer_cash from customers_table where customer_id = {current_customer_id};')
-                result = cursor.fetchone()
-            except: pass
+    Withdraw command to update CASH at customer_id.\n
+    Cash -= cash_input for user.\n
+    takes 0 args, only asks inside of function.
+    '''
+        try: #check how much money customer have
+            self.cursor.execute(f'select customer_cash from customers_table where customer_id = {self.current_customer_id};')
+            result = self.cursor.fetchone()
+        except: pass
 
-            try:
-                cash = int(input('How much do you want to withdraw (input integer)?: '))
-                if cash < 0 or int(result[0]) - cash < 0:
-                    print("You cant take debt here!")
-                    cash = str(cash)
-                    cash = "nope"
-            except:
-                print("Withdraw Only integers and positive numbers, ATM can not hold cents")
+        try:
+            cash = int(input('How much do you want to withdraw (input integer)?: '))
+            if cash < 0 or int(result[0]) - cash < 0: #type: ignore
+                print("You cant take debt here!")
+                cash = str(cash)
+                cash = "nope"
+        except:
+            print("Withdraw Only integers and positive numbers, ATM can not hold cents")
 
-            cursor.execute(f'update customers_table set customer_cash = customer_cash - {cash} where customer_id = "{current_customer_id}";')
-            printCashAmountById(current_customer_id, cursor)
+        self.cursor.execute(f'update customers_table set customer_cash = customer_cash - {cash} where customer_id = "{self.current_customer_id}";') #type: ignore
+        self.printCashAmount()
 
 
-        def send(cursor) -> None:
-            '''
-        Send command to update CASH at customer_id[i] and customer_id[j].\n
-        Sends cash from one user to another.\n
-        takes 0 args, only asks inside of function.
+    def send(self) -> None:
         '''
-            cursor.execute('select customer_name from customers_table;')
-            
-            for i, name in enumerate(cursor.fetchall(), start=1):
-                print(f"{i}) {name}")
-            try:
-                taker_id = int(input('Send money to who? (taker id): '))
-            except: pass
-
-            try: #check how much money customer do have
-                cursor.execute(f'select customer_cash from customers_table where customer_id = {current_customer_id};')
-                cash_on_sender_card = cursor.fetchone()
-            except: pass
-
-            try:
-                cash_to_send = int(input('How much do you want to withdraw (input integer)?: '))
-                if cash_to_send < 0 or int(cash_on_sender_card[0]) - cash_to_send < 0:
-                    print("You cant take debt here!")
-                    cash_to_send = str(cash_to_send)
-                    cash_to_send = "nope"
-            except:
-                print("Only integers and positive numbers, ATM can not hold cents")
-
-            a = f'update customers_table set customer_cash = customer_cash - {cash_to_send} where customer_id = "{current_customer_id}";'
-            b = f'update customers_table set customer_cash = customer_cash + {cash_to_send} where customer_id = "{taker_id}";'
-
-            cursor.execute(a)
-            cursor.execute(b)
-
-            printCashAmountById(current_customer_id, cursor)
-            printCashAmountById(taker_id, cursor)
-
-
-        def printCashAmountById(current_customer_id, cursor) -> None:
-            '''
-        Select command to print in terminal\n
-        Print in terminal amount of money of customer_id \n
-        Takes 1 positional arg: customer_id e.g. "1"
-        '''
-            cursor.execute(f'select customer_cash from customers_table where customer_id = "{current_customer_id}";')
-
-            result = cursor.fetchone()
-            print(f"#{current_customer_id}'s cash is now {result[0]}")
-            
-
-
-        def main() -> None:
-                '''
-            Gui command to print atm menu in terminal.\n
-            Takes int 1-4 to reach other commands, if input is incorrect trying again.
-            '''
-                while True:
-                    try:
-                        cursor = db.cursor()
-                        match int(input(f'\n\n\n\n\n===ATM===\nHello, {getCustomerNameById(cursor, current_customer_id)}\n1) Deposit\n2) Withdraw\n3) Send cash\n4) Exit\n')):
-                            case 1: deposit(cursor); input("Press ENTER")
-                            case 2: withdraw(cursor); input("Press ENTER")
-                            case 3: send(cursor); input("Press ENTER")
-                            case 4: break
-                        cursor.execute("commit;")
-                        cursor.close()
-                    except Exception as err: 
-                        print(f'[BUG] {err}')
-                    
-
+    Send command to update CASH at customer_id[i] and customer_id[j].\n
+    Sends cash from one user to another.\n
+    takes 0 args, only asks inside of function.
+    '''
+        self.cursor.execute('select customer_name from customers_table;')
         
-        main()
+        for i, name in enumerate(self.cursor.fetchall(), start=1):
+            print(f"{i}) {name}")
+        try:
+            taker_id = int(input('Send money to who? (taker id): '))
+        except: pass
+
+        try: #check how much money customer do have
+            self.cursor.execute(f'select customer_cash from customers_table where customer_id = {self.current_customer_id};')
+            cash_on_sender_card = self.cursor.fetchone()
+        except: pass
+
+        try:
+            cash_to_send = int(input('How much do you want to withdraw (input integer)?: '))
+            if cash_to_send < 0 or int(cash_on_sender_card[0]) - cash_to_send < 0: #type: ignore
+                print("You cant take debt here!")
+                cash_to_send = str(cash_to_send)
+                cash_to_send = "nope"
+        except:
+            print("Only integers and positive numbers, ATM can not hold cents")
+
+        a = f'update customers_table set customer_cash = customer_cash - {cash_to_send} where customer_id = "{self.current_customer_id}";' # type: ignore
+        b = f'update customers_table set customer_cash = customer_cash + {cash_to_send} where customer_id = "{taker_id}";' # type: ignore
+
+        self.cursor.execute(a)
+        self.cursor.execute(b)
+
+        self.printCashAmount()
+        self.printCashAmountById(taker_id) #type: ignore
+
+
+    def printCashAmountById(self, current_customer_id) -> None:
+        '''
+    Select command to print in terminal\n
+    Print in terminal amount of money of customer_id \n
+    Takes 1 positional arg: customer_id e.g. "1"
+    '''
+        self.cursor.execute(f'select customer_cash from customers_table where customer_id = "{current_customer_id}";')
+
+        result = self.cursor.fetchone()
+        print(f"#{current_customer_id}'s cash is now {result[0]}")
+        
+
+    def printCashAmount(self) -> None:
+        '''
+    Select command to print in terminal\n
+    Print in terminal amount of money of customer_id \n
+    Takes 1 positional arg: customer_id e.g. "1"
+    '''
+        self.cursor.execute(f'select customer_cash from customers_table where customer_id = "{self.current_customer_id}";')
+
+        result = self.cursor.fetchone()
+        print(f"#{self.current_customer_id}'s cash is now {result[0]}")
+        
+
+    def main(self) -> None:
+            '''
+        Gui command to print atm menu in terminal.\n
+        Takes int 1-4 to reach other commands, if input is incorrect trying again.
+        '''
+            while True:
+                try:
+                    self.cursor = db.cursor()
+                    match int(input(f'\n\n\n\n\n===ATM===\nHello, {self.getCustomerNameById()}\n1) Deposit\n2) Withdraw\n3) Send cash\n4) Exit\n')):
+                        case 1: self.deposit(); input("Press ENTER")
+                        case 2: self.withdraw(); input("Press ENTER")
+                        case 3: self.send(); input("Press ENTER")
+                        case 4: break
+                    self.cursor.execute("commit;")
+                    self.cursor.close()
+                except Exception as err: 
+                    print(f'[BUG] {err}')
          
 
 if __name__ == "__main__":
